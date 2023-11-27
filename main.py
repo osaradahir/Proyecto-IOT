08fd4ddaff293034ada315ccafa49dd25f028c35
oscar <1722110455@utectulancingo.edu.mx>
2023-11-27 13:39:26 -0600

    API

--- main.py
+++ main.py
@@ -48,16 +48,6 @@ async def obtener_dispositivos():
 
 @app.get("/dispositivos/{id_dispositivo}")
 async def obtener_dispositivo(id_dispositivo: int):
-    """Obtiene un dispositivo por su id."""
-    c = conn.cursor()
-    c.execute('SELECT * FROM dispositivos WHERE id_dispositivo = ?', (id_dispositivo,))
-    dispositivo = None
-    for row in c:
-        dispositivo = {"id_dispositivo": row[0], "dispositivo": row[1], "valor": row[2]}
-    return dispositivo
-
-@app.get("/dispositivos/{id_dispositivo}/{valor}")
-async def obtener_dispositivo(id_dispositivo: int, valor: int):
     """Obtiene el valor de un dispositivo por su ID."""
     c = conn.cursor()
     c.execute('SELECT valor FROM dispositivos WHERE id_dispositivo = ?', (id_dispositivo,))
